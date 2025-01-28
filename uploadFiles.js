baee1f792bafa35638655baea103d445e094d710
Gianluca Donato <gian.dnt@gmail.com>
2025-01-28 22:30:01 +0100

    upload files from different folders

--- uploadFiles.js
+++ uploadFiles.js
@@ -2,8 +2,13 @@ const path = require("node:path");
 const fs = require("node:fs");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
+const NOW = new Date().toISOString().split("Z")[0];
 const BUCKET_NAME = "elmeteo";
-const FILES_FOLDER = path.join(__dirname, "./files");
+const FILES_FOLDERS = [
+  "/Users/gian/Documents/folder1",
+  "/Users/gian/Documents/folder2",
+  "/Users/gian/Desktop/folder3",
+];
 
 const s3Client = new S3Client({
   region: "eu-central-1",
@@ -13,8 +18,8 @@ const s3Client = new S3Client({
   },
 });
 
-const parseFilePath = (files, path) => {
-  const fullPath = `${FILES_FOLDER}/${path}`;
+const parseFilePath = (folderPath, files, path) => {
+  const fullPath = `${folderPath}/${path}`;
   const stat = fs.statSync(fullPath);
   if (stat.isFile()) {
     const fileContent = fs.readFileSync(fullPath);
@@ -24,39 +29,42 @@ const parseFilePath = (files, path) => {
     });
   }
   const keys = fs.readdirSync(fullPath);
-  return keys.map((key) => parseFilePath(files, `${path}/${key}`));
+  return keys.map((key) => parseFilePath(folderPath, files, `${path}/${key}`));
 };
 
 const uploadFilesToBucket = async ({ folderPath, bucketName }) => {
-  console.log(`Uploading files from ${folderPath}\n`);
+  console.log(`\nUploading files from ${folderPath}...`);
   const files = [];
   const keys = fs.readdirSync(folderPath);
   keys
     .filter((key) => !key.startsWith(".gitignore"))
-    .forEach((key) => parseFilePath(files, key));
+    .forEach((key) => parseFilePath(folderPath, files, key));
 
-  const now = new Date().toISOString().split("Z")[0];
+  const folderName = folderPath.split("/").pop();
   for (const file of files) {
     await s3Client.send(
       new PutObjectCommand({
         Bucket: bucketName,
         Body: file.Body,
-        Key: `${now}/${file.Key}`,
+        Key: `${NOW}/${folderName}/${file.Key}`,
       })
     );
-    console.log(`${file.Key} - uploaded.`);
+    // console.log(`${file.Key} - uploaded.`);
   }
-  console.log("\n✅ Done!");
-  console.log(`Uploaded ${files.length} files to ${bucketName}.`);
+  console.log(`Uploaded ${files.length} files to ${bucketName}/${NOW}/${folderName}.`);
 };
 
 const main = async () => {
   try {
-    await uploadFilesToBucket({
-      bucketName: BUCKET_NAME,
-      folderPath: FILES_FOLDER,
-    });
+    for (const folderPath of FILES_FOLDERS) {
+      await uploadFilesToBucket({
+        bucketName: BUCKET_NAME,
+        folderPath,
+      });
+      console.log(`✅ Upload ${folderPath} done!`);
+    }
   } catch (err) {
+    console.log("🚨 Error uploading files to bucket");
     console.error(err);
   }
 };
